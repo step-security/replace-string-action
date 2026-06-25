82af2db31e4eb9e4bee9eebbe3e8edaf0402ee48
Raj-StepSecurity <rajkumar@stepsecurity.io>
2026-06-25 11:17:59 +0530

    feat: onboarding replace-string-action

---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+
+try {
+  const pattern = core.getInput('pattern');
+  const string = core.getInput('string');
+  const replaceWith = core.getInput('replace-with', { trimWhitespace: false });
+  const flags = core.getInput('flags');
+
+  const regex = new RegExp(pattern, flags);
+
+  core.setOutput('replaced', string.replace(regex, replaceWith));
+} catch (error) {
+  core.setFailed(error.message);
+}
